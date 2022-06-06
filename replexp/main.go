12b12f7e8fa4a0a81a1deef18612ab997ac60ead
Patrick Bucher <patrick.bucher@mailbox.org>
2022-06-06 14:39:54 +0200

    simple regexp matcher repl

---
+++ replexp/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s [regexp]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	var err error
+	pattern, err := regexp.Compile(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "compile regexp `%s`: %v\n", os.Args[1], err)
+		os.Exit(2)
+	}
+
+	input := bufio.NewReader(os.Stdin)
+	var line []byte
+	for ; err != io.EOF; line, err = input.ReadBytes('\n') {
+		if len(line) == 0 {
+			continue
+		}
+		if pattern.Match(line) {
+			fmt.Println("-> match")
+		} else {
+			fmt.Println("-> no match")
+		}
+	}
+}
