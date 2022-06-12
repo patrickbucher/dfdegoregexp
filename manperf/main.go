9370121e7b3139d05d5bb56da27462ca436cc2b4
Patrick Bucher <patrick.bucher@mailbox.org>
2022-06-12 20:09:28 +0200

    error handling

--- manperf/main.go
+++ manperf/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s [command] [arguments]", os.Args[0])
+		os.Exit(1)
+	}
+
 	lines := dr.CommandOutput(os.Args[1], os.Args[1:]...)
 	fmt.Print(strings.Join(dr.ExtractSectionsBad(lines), "\n"))
 
